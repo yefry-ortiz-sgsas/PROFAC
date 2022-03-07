95274625d4c5b23ccaaf154b5faed17e2d411c8a
cleeper24 <fer504guevara@gmail.com>
2022-03-07 02:41:35 -0600

    implementacion de plantilla html

--- SISTEMA/profac-app/resources/views/layouts/app.blade.php
+++ SISTEMA/profac-app/resources/views/layouts/app.blade.php
@@ -7,40 +7,107 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
         @livewireStyles
+          <!-- Styles -->
+          <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+        <link href="{{ asset('font-awesome/css/font-awesome.css') }}" rel="stylesheet">
+
+        <!-- Toastr style -->
+        <link href="{{ asset('css/plugins/toastr/toastr.min.css') }}" rel="stylesheet">
+
+        <!-- Gritter -->
+        <link href="{{ asset('js/plugins/gritter/jquery.gritter.css') }}" rel="stylesheet">
+
+        <link href="{{ asset('css/animate.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+
+        <!-- Fonts -->
+        <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"> -->
+
+      
+
+       
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased">
+    <body >
         <x-jet-banner />
 
-        <div class="min-h-screen bg-gray-100">
+        <div class="">
+
+          
+        <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i></a>    
             @livewire('navigation-menu')
 
+
             <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+          
 
             <!-- Page Content -->
-            <main>
+       
+        <div id="page-wrapper" class="gray-bg dashbard-1">
+        <div class="row border-bottom">
+
+            <main >
                 {{ $slot }}
             </main>
+
+
+        </div>
+        </div>
+            
+
+          
+            
         </div>
 
         @stack('modals')
 
         @livewireScripts
+
+    <script src="{{ asset('js/jquery-3.1.1.min.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
+    <script src="{{ asset('js/plugins/metisMenu/jquery.metisMenu.js') }}"></script>
+    <script src="{{ asset('js/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>    
+
+    
+    <!-- Flot -->
+    <script src="{{ asset('js/plugins/flot/jquery.flot.js') }}"></script>
+    <script src="{{ asset('js/plugins/flot/jquery.flot.tooltip.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/flot/jquery.flot.spline.js') }}"></script>
+    <script src="{{ asset('js/plugins/flot/jquery.flot.resize.js') }}"></script>
+    <script src="{{ asset('js/plugins/flot/jquery.flot.pie.js') }}"></script>
+
+    <!-- Peity -->
+    <script src="{{ asset('js/plugins/peity/jquery.peity.min.js') }}"></script>
+    <script src="{{ asset('js/demo/peity-demo.js') }}"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="{{ asset('js/inspinia.js') }}"></script>
+    <script src="{{ asset('js/plugins/pace/pace.min.js') }}"></script>
+
+    <!-- jQuery UI -->
+    <script src="{{ asset('js/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+
+    <!-- GITTER -->
+    <script src="{{ asset('js/plugins/gritter/jquery.gritter.min.js') }}"></script>
+
+    <!-- Sparkline -->
+    <script src="{{ asset('js/plugins/sparkline/jquery.sparkline.min.js') }}"></script>
+
+    <!-- Sparkline demo data  -->
+    <script src="{{ asset('js/demo/sparkline-demo.js') }}"></script>
+
+    <!-- ChartJS-->
+    <script src="{{ asset('js/plugins/chartJs/Chart.min.js') }}"></script>
+
+    <!-- Toastr -->
+    <script src="{{ asset('js/plugins/toastr/toastr.min.js') }}"></script>
+
     </body>
 </html>
