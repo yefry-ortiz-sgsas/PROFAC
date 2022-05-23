4834ffc36b623de9121ab6ab83a99408941542b1
Yefry-yeff <55094597+Yefry-yeff@users.noreply.github.com>
2022-05-22 21:33:21 -0500

    z

--- SISTEMA/profac-app/resources/views/pdf/retencion.blade.php
+++ SISTEMA/profac-app/resources/views/pdf/retencion.blade.php
@@ -2,24 +2,22 @@
 <html>
 
 <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <style>
         .color-red {
             color: red;
         }
 
         body {
-            margin: 0px;
+            margin: -45px;
             padding: 0px;
-            background-image: url('img/membrete/membrete3.png'); 
+            background-image: url('img/membrete/membrete1.jpg');
             background-repeat: no-repeat;
-            background-size: 100% 100%;
+            background-size: 200% 200%;
             background-size: cover;
-            /*background-attachment: fixed;*/
-           
-            width: 100%!important;
-  
-           
-            /* background-color: aqua; */
+
+            width: 115%!important;
+
         }
 
     </style>
@@ -27,10 +25,36 @@
 </head>
 
 <body>
-
+{{--
     <h1>{{ $titulo }}</h1>
-    <h2 class="color-red">sub carpeta</h2>
+    <h2 class="color-red">sub carpeta</h2> --}}
 
+        <br><br><br><br><br><br><br><br><br><br><br><br>
+        <hr>
+
+
+
+
+
+    <div class="pruebaFondo">
+        <div class="card" style="justify-content: center;  width:30rem;">
+            <div class="card-header">
+            Featured
+            </div>
+            <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+
+
+
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
