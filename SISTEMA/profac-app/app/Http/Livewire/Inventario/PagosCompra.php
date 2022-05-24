c77e457c2c8cb8bfbfd2619a2be0a409f8cf4d34
Yefry-yeff <55094597+Yefry-yeff@users.noreply.github.com>
2022-05-23 21:25:12 -0500

    facturacion pdf

--- SISTEMA/profac-app/app/Http/Livewire/Inventario/PagosCompra.php
+++ SISTEMA/profac-app/app/Http/Livewire/Inventario/PagosCompra.php
@@ -34,16 +34,16 @@ class PagosCompra extends Component
 
 
         $datosCompra = DB::SELECTONE("
-        select 
-        numero_factura, 
+        select
+        numero_factura,
         numero_orden as 'numero_compra' ,
         proveedores.nombre
-        from compra 
+        from compra
         inner join proveedores
         on compra.proveedores_id = proveedores.id
         where compra.id = ".$id
         );
- 
+
         return view('livewire.inventario.pagos-compra',compact('id','datosCompra'));
     }
 
@@ -57,17 +57,17 @@ class PagosCompra extends Component
                 'monto' => 'required',
                 'fecha_pago' => 'required',
                 'img_pago' => 'required|mimes:png,jpeg,jpg,pdf'
-                
-    
+
+
             ], [
                 'numero_factura' => 'Numero de factura es requerido',
-                'numero_compra' => 'Numero de compra es requerido',                
+                'numero_compra' => 'Numero de compra es requerido',
                 'proveedor' => 'El proveedor es requerido',
                 'monto' => 'El monto es requerido',
                 'fecha_pago' => 'La fecha de pago es requerida',
                 'img_pago' => 'Formato de imagen invalido'
             ]);
-    
+
             if ($validator->fails()) {
                 return response()->json([
                     'mensaje' => 'Ha ocurrido un error.',
@@ -75,7 +75,7 @@ class PagosCompra extends Component
                 ], 402);
             }
 
-           
+
 
             $totalCompra = DB::SELECTONE("select total from compra where id=".$request->compraId);
             $subTotalCompra = DB::SELECTONE("select sub_total from compra where id=".$request->compraId);
@@ -90,7 +90,7 @@ class PagosCompra extends Component
                     "icon" => "warning",
                     "text"=>"El total de la compra ya ha sido cancelado. no puede realizar mas abonos a la deuda.",
                     "title"=>"Advertencia!"
-                    
+
                 ],200);
 
             }
@@ -102,7 +102,7 @@ class PagosCompra extends Component
                     "icon" => "warning",
                     "text"=>"El monto de pago, supera la deuda existente, el registro de pago no puede ser procesado.",
                     "title"=>"Advertencia!"
-                    
+
                 ],200);
 
             }
@@ -111,9 +111,9 @@ class PagosCompra extends Component
 
                 $file = $request->file('img_pago');
                 $name = 'IMG_'. time()."-". '.' . $file->getClientOriginalExtension();
-                $path = public_path() . '/documentos_pagos';   
+                $path = public_path() . '/documentos_pagos';
                 $file->move($path, $name);
-                        
+
                 $registroPago = new ModelPagoCompra;
                 $registroPago->monto = $request->monto;
                 $registroPago->fecha = $request->fecha_pago;
@@ -128,7 +128,7 @@ class PagosCompra extends Component
 
                 $faltantePago = round($totalCompra->total,2) - round($totalPagos->monto,2);
 
-                
+
 
                 if($request->retencionEstado == 0){
 
@@ -146,26 +146,26 @@ class PagosCompra extends Component
                     $compra->retenciones_id = 2;
                     $compra->save();
                 }
-               
-               
-                
 
-          
-            
+
+
+
+
+
             DB::commit();
             return response()->json([
                 "icon" => "success",
                 "text" => "Registro de pago realizo con exito!",
                 "title"=>"Exito!"
 
-                
+
             ],200);
         } catch (QueryException $e) {
             DB::rollback();
 
             $carpetaPublic = public_path();
-            $path = $carpetaPublic.'/documentos_pagos/'.$name;  
-    
+            $path = $carpetaPublic.'/documentos_pagos/'.$name;
+
             File::delete($path);
 
             return response()->json([
@@ -179,22 +179,22 @@ class PagosCompra extends Component
        try {
 
         $compra = DB::SELECTONE("select total, debito, if(monto_retencion is null ,0,monto_retencion) as 'monto_retencion' from compra where id=".$request->idCompra);
-        
+
        return response()->json([
            "compra"=>$compra,
-           
+
        ]);
 
        } catch (QueryException $e) {
        return response()->json([
-           'message' => 'Ha ocurrido un error', 
+           'message' => 'Ha ocurrido un error',
            'error' => $e
        ]);
        }
     }
 
     public function listarPagos($id){
-        
+
 
        try {
 
@@ -209,7 +209,7 @@ class PagosCompra extends Component
             pago_compra.created_at,
             compra.numero_factura,
             compra.numero_orden,
-            compra.id as 'idCompra'        
+            compra.id as 'idCompra'
         FROM compra
         inner join pago_compra
         on pago_compra.compra_id = compra.id
@@ -227,7 +227,7 @@ class PagosCompra extends Component
             <div class="text-center">
                 <button class="btn btn-danger " onclick="confirmarEliminarPago('.$listaPagos->id.')"><i class="fa-solid fa-trash-can "></i></button>
             </div>
-          
+
 
 
 
@@ -240,7 +240,7 @@ class PagosCompra extends Component
             <div class="text-center ">
                     <a href="/documentos_pagos/'.$listaPagos->url_img.'" target="_blank" class=""><i class="fa-solid fa-file-pdf text-danger" style="font-size: 2rem;"></i></a>
             </div>
-          
+
 
 
 
@@ -252,7 +252,7 @@ class PagosCompra extends Component
 
        } catch (QueryException $e) {
        return response()->json([
-           'message' => 'Ha ocurrido un error', 
+           'message' => 'Ha ocurrido un error',
            'error' => $e
        ]);
        }
@@ -262,12 +262,12 @@ class PagosCompra extends Component
     public function eliminarPago(Request $request){
         try {
 
-            
+
 
             $pago = ModelPagoCompra::find($request->idPago);
             $pago->estado_id=2;
             $pago->users_id_elimina = Auth::user()->id;
-            $pago->fecha_eliminado = now();        
+            $pago->fecha_eliminado = now();
             $pago->save();
 
             $totalCompra = DB::SELECTONE("select total from compra where id=".$pago->compra_id);
@@ -277,7 +277,7 @@ class PagosCompra extends Component
 
             $faltantePago = round($totalCompra->total,2) - round($totalPagos->monto,2);
 
-           
+
             if($numeroPagos->numero_pagos == 0){
                 $compra = ModelCompra::find($pago->compra_id);
                 $compra->debito = round($faltantePago,2);
@@ -293,13 +293,13 @@ class PagosCompra extends Component
 
 
 
-    
+
         return response()->json([
             "message" => "Eliminado con exito!"
         ]);
         } catch (QueryException $e) {
         return response()->json([
-            'message' => 'Ha ocurrido un error al eliminar el pago.', 
+            'message' => 'Ha ocurrido un error al eliminar el pago.',
             'error' => $e
         ]);
         }
@@ -316,7 +316,7 @@ class PagosCompra extends Component
             ]);
        } catch (QueryException $e) {
        return response()->json([
-           'message' => 'Ha ocurrido un error', 
+           'message' => 'Ha ocurrido un error',
            'error' => $e
        ]);
        }
@@ -328,16 +328,16 @@ class PagosCompra extends Component
         ];
 
         /*$pdf = PDF::loadView('/pdf/retencion', $data)
-        ->stream('archivo.pdf');   
+        ->stream('archivo.pdf');
         */
-        
+
         //view()->share('/pdf/retencion',$data);
-       
-        $pdf = PDF::loadView('/pdf/retencion', $data)->setPaper('letter');
+
+        $pdf = PDF::loadView('/pdf/factura', $data)->setPaper('letter');
         // download PDF file with download method
         return $pdf->stream('pdf_file.pdf');
-        
+
         // return view('/pdf/retencion')->with($data);
     }
-    
+
 }
---
+++ SISTEMA/profac-app/resources/views/pdf/factura.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        .color-red {
+            color: red;
+        }
+        p {
+        font-size: 12px;
+        }
+        body {
+            margin: -45px;
+            padding: 0px;
+            background-image: url('img/membrete/membrete2.jpg');
+            background-repeat: no-repeat;
+            background-size: 200% 200%;
+            background-size: cover;
+
+            width: 115%!important;
+
+        }
+
+    </style>
+    <title>FACTURA</title>
+</head>
+
+<body>
+
+
+
+    <div class="pruebaFondo">
+        <div class="card border border-dark" style="position:absolute;left:0px;  top:180px; width:45rem; height:5.5rem;">
+            <div class="card-header">
+               <b>Retención No. 0000-001-05-00000657</b>
+              </div>
+            <div class="card-body">
+                <p class="card-text" style="position:absolute;left:20px;  top:50px;"><b>Reistro tributario: 0801198892837</b></p>
+                <p class="card-text" style="position:absolute;left:410px;  top:50px;"><b>CAI: 08011988937783VG35V53Y53B3588GH52837</b></p>
+                <p class="card-text" style="position:absolute;left:20px;  top:65px;"><b>Fecha límite de emisión: 11/03/2022</b></p>
+                <p class="card-text" style="position:absolute;left:310px;  top:65px;"><b>Rango autorizado: 08011988937783VG35V5 - 3Y53B3588GH52837</b></p>
+            </div>
+        </div>
+
+        <div class="card border border-dark" style="position:absolute;left:0px;  top:300px; width:45rem; height:5.5rem;">
+            <div class="card-body">
+                <p class="card-text" style="position:absolute;left:20px;  top:10px;"><b>Proveedor:</b> PACASA CALPULES</p>
+                <p class="card-text" style="position:absolute;left:350px;  top:10px;"><b>Fecha:</b> 11/03/2022</p>
+                <p class="card-text" style="position:absolute;left:550px;  top:10px;"><b>Porcentaje:</b> 1.00</p>
+                <p class="card-text" style="position:absolute;left:20px;  top:35px;"><b>CAI:</b> 08011988937783VG35V53Y53B3588GH52837</p>
+                <p class="card-text" style="position:absolute;left:550px;  top:35px;"><b>RTN:</b> 081210515818514818</p>
+                <p class="card-text" style="position:absolute;left:20px;  top:60px;"><b>Comentarios:</b> RETENCION DEL 1%</p>
+            </div>
+        </div>
+
+        <div class="card border border-dark" style="position:absolute;left:0px;  top:420px; width:45rem; height:4rem;">
+            <div class="card-body">
+                <p class="card-text" style="position:absolute;left:20px;  top:10px;"><b>Linea</b></p>
+                <p class="card-text" style="position:absolute;left:150px;  top:10px;"><b>Descripción</b></p>
+                <p class="card-text" style="position:absolute;left:450px;  top:10px;"><b>Valor</b></p>
+                <p class="card-text" style="position:absolute;left:600px;  top:10px;"><b>Retenido</b></p>
+                <p class="card-text" style="position:absolute;left:30px;  top:30px;">1</p>
+                <p class="card-text" style="position:absolute;left:90px;  top:30px;">FACTURA N 010-001-01-00418759 / 16/12/2021</p>
+                <p class="card-text" style="position:absolute;left:455px;  top:30px;">22,412.50</p>
+                <p class="card-text" style="position:absolute;left:605px;  top:30px;">224.13</p>
+            </div>
+        </div>
+
+        <div class="card border border-dark" style="position:absolute;left:0px;  top:520px; width:45rem; height:3rem;">
+            <div class="card-body">
+                <p class="card-text" style="position:absolute;left:50px;  top:10px;">"""doscientos veinticuatro lempiras con doce centavos"""</p>
+                <p class="card-text" style="position:absolute;left:450px;  top:10px;"><b><h5>Valor Retenido: 224.12</h5></b></p>
+            </div>
+        </div>
+
+        <div style="position:absolute;left:0px;  top:820px;  width:45rem;">
+                <p class="card-text" style="position:absolute;left:20px;  top:10px;">_______________________________________</p>
+                <p class="card-text" style="position:absolute;left:450px;  top:10px;">_______________________________________</p>
+                <p class="card-text" style="position:absolute;left:20px;  top:25px; ">FACTURA N 010-001-01-00418759 / 16/12/2021</p>
+                <p class="card-text" style="position:absolute;left:495px;  top:25px;">DISTRIBUCIONES VALENCIA</p>
+        </div>
+    </div>
+
+
+
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
