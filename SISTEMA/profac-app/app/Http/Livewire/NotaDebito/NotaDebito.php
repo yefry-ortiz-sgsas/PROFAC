cb5531646fdeb9b1ea3cd9f5f66c035f70377f3f
Yefry-yeff <55094597+Yefry-yeff@users.noreply.github.com>
2023-01-08 15:27:28 -0600

    mnnmnm

--- SISTEMA/profac-app/app/Http/Livewire/NotaDebito/NotaDebito.php
+++ SISTEMA/profac-app/app/Http/Livewire/NotaDebito/NotaDebito.php
@@ -22,36 +22,37 @@ class NotaDebito extends Component
         try {
 
             $listaFacturas = DB::SELECT("
-                select
-                factura.id as id,
-                @i := @i + 1 as contador,
-                numero_factura,
-                factura.cai as correlativo,
-                A.cai as cai,
-                fecha_emision,
-                cliente.nombre,
-                tipo_pago_venta.descripcion,
-                fecha_vencimiento,
-                FORMAT(sub_total,2) as sub_total,
-                FORMAT(isv,2) as isv,
-                FORMAT(total,2) as total,
-                factura.credito,
-                users.name as creado_por,
-                (select if(sum(monto) is null,0,sum(monto)) from pago_venta where estado_venta_id = 1   and factura_id = factura.id ) as monto_pagado,
-                factura.estado_venta_id
+            select
+            factura.id as id,
+            @i := @i + 1 as contador,
+            numero_factura,
+            factura.cai as correlativo,
+            A.cai as cai,
+            fecha_emision,
+            cliente.nombre,
+            tipo_pago_venta.descripcion,
+            fecha_vencimiento,
+            FORMAT(sub_total,2) as sub_total,
+            FORMAT(isv,2) as isv,
+            FORMAT(total,2) as total,
+            factura.credito,
+            users.name as creado_por,
+            (select if(sum(monto) is null,0,sum(monto)) from pago_venta where estado_venta_id = 1   and factura_id = factura.id ) as monto_pagado,
+            factura.estado_venta_id
 
-                from factura
-                inner join cliente
-                on factura.cliente_id = cliente.id
-                inner join tipo_pago_venta
-                on factura.tipo_pago_id = tipo_pago_venta.id
-                inner join users
-                on factura.vendedor = users.id
-                inner join cai A
-                on factura.cai_id= A.id
-                cross join (select @i := 0) r
-                where ( YEAR(factura.created_at) >= (YEAR(NOW())-2) )and factura.estado_factura_id=1 and factura.estado_venta_id<>2 and factura.tipo_pago_cobro = 3
-                order by factura.created_at desc
+            from factura
+            inner join cliente
+            on factura.cliente_id = cliente.id
+            inner join tipo_pago_venta
+            on factura.tipo_pago_id = tipo_pago_venta.id
+            inner join users
+            on factura.vendedor = users.id
+            inner join cai A
+            on factura.cai_id= A.id
+            inner join pago_venta on (pago_venta.factura_id = factura.id)
+            cross join (select @i := 0) r
+            where ( YEAR(factura.created_at) >= (YEAR(NOW())-2) )and factura.estado_factura_id=1 and factura.estado_venta_id<>2 and pago_venta.tipo_pago_id = 3
+            order by factura.created_at desc
             ");
 
 
@@ -98,7 +99,7 @@ class NotaDebito extends Component
             ->addColumn('estado_cobro', function ($listaFacturas) {
 
 
-                  if( round($listaFacturas->monto_pagado,2) >= str_replace(",","",$listaFacturas->total) ){
+                if( round($listaFacturas->monto_pagado,2) >= str_replace(",","",$listaFacturas->total) ){
 
                     return
                     '
