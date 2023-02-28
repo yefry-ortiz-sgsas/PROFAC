6c853160cde0cc4447f8938085e25610dae2ebc4
Yefry-yeff <55094597+Yefry-yeff@users.noreply.github.com>
2023-02-28 10:27:02 -0600

    RESOLUCION NOTADEBITO

--- SISTEMA/profac-app/app/Models/NotaDebito/notaDebito.php
+++ SISTEMA/profac-app/app/Models/NotaDebito/notaDebito.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class notaDebito extends Model
 {
     use HasFactory;
-    protected $table = 'notaDebito';
+    protected $table = 'notadebito';
     protected $primaryKey = 'id';
     protected $fillable = ['factura_id', 'montoNotaDebito_id','monto_asignado', 'fechaEmision', 'motivoDescripcion', 'cai_ndebito','numeroCai', 'correlativoND', 'estado_id','estado_nota_dec','users_registra_id'];
 }
