c0afdf507fa1390bd056d015f54164ce72650bd7
cleeper20 <39780147+cleeper20@users.noreply.github.com>
2023-03-07 00:21:04 -0600

    salto de linea

--- SISTEMA/profac-app/app/Http/Livewire/Ventas/ListadoFacturasND.php
+++ SISTEMA/profac-app/app/Http/Livewire/Ventas/ListadoFacturasND.php
@@ -81,8 +81,12 @@ class ListadoFacturasND extends Component
                             </li>
                             
                             <li>
-                            <a class="dropdown-item" target="_blank"  href="/factura/cooporativo/'.$listaFacturas->id.'"> <i class="fa-solid fa-print text-info"></i> Imprimir Factura </a>
-                            </li>    
+                            <a class="dropdown-item" target="_blank"  href="/factura/cooporativo/'.$listaFacturas->id.'"> <i class="fa-solid fa-print text-info"></i> Imprimir Factura Original </a>
+                            </li>   
+                            
+                            <li>
+                            <a class="dropdown-item" target="_blank"  href="/factura/cooporativoCopia/'.$listaFacturas->id.'"> <i class="fa-solid fa-print text-info"></i> Imprimir Factura Copia </a>
+                            </li>
     
                             <li>
                             <a class="dropdown-item"  onclick="anularVentaConfirmar('.$listaFacturas->id.')" > <i class="fa-solid fa-ban text-danger"></i> Anular Factura </a>
