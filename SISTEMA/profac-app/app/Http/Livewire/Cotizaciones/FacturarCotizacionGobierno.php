0e7ac44dd7f37d82bae8f278e11e25a7b1e1e240
cleeper20 <39780147+cleeper20@users.noreply.github.com>
2023-03-26 22:36:34 -0600

    correcion en cotizaciones

--- SISTEMA/profac-app/app/Http/Livewire/Cotizaciones/FacturarCotizacionGobierno.php
+++ SISTEMA/profac-app/app/Http/Livewire/Cotizaciones/FacturarCotizacionGobierno.php
@@ -167,7 +167,7 @@ class FacturarCotizacionGobierno extends Component
                     <label for="precio' . $i . '" class="sr-only">Precio</label>
                     <input value="' . $producto->precio_unidad . '" type="number" placeholder="Precio Unidad" id="precio' . $i . '"
                         name="precio' . $i . '" class="form-control"  data-parsley-required step="any"
-                        autocomplete="off" min="' . $producto->precio_base . '" onchange="calcularTotales(precio' . $i . ',cantidad' . $i . ',' . $producto->isv . ',unidad' . $i . ',' . $i . ',restaInventario' . $i . ')">
+                        autocomplete="off" min="' . $producto->precio_base . '" onchange="calcularTotales(precio' . $i . ',cantidad' . $i . ',' . $producto->isvTblProducto . ',unidad' . $i . ',' . $i . ',restaInventario' . $i . ')">
                 </div>
 
                 <div class="form-group col-12 col-sm-12 col-md-1 col-lg-1 col-xl-1">
