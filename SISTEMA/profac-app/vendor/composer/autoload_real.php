6e250236321b703e871a9bbe8af7b88abac373c5
Yefry-yeff <55094597+Yefry-yeff@users.noreply.github.com>
2023-08-24 11:14:14 -0600

    Actualizando npm y composer

--- SISTEMA/profac-app/vendor/composer/autoload_real.php
+++ SISTEMA/profac-app/vendor/composer/autoload_real.php
@@ -65,11 +65,16 @@ class ComposerAutoloaderInit2bc7b4654d0cb40359b587613c23209f
     }
 }
 
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
 function composerRequire2bc7b4654d0cb40359b587613c23209f($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
         $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
