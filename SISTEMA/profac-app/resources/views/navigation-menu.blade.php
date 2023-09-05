cf0b94a8a6e96df96e40b29b62cba222953f83fb
Yefry-yeff <55094597+Yefry-yeff@users.noreply.github.com>
2023-09-04 20:10:26 -0600

    Arreglito en el menu

--- SISTEMA/profac-app/resources/views/navigation-menu.blade.php
+++ SISTEMA/profac-app/resources/views/navigation-menu.blade.php
@@ -19,7 +19,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                       <b>{{ __('DISTRIBUCIONES VALENCIA  - DESARROLLO') }}</b>
+                       <b>{{ __('DISTRIBUCIONES VALENCIA  - DESARROLLO - V2.2.3') }}</b>
                     </x-jet-nav-link>
                 </div>
             </div>
